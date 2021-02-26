47971c59787e0eff11c26a337309f82e301bf6bd
Volodymyr Buts <wowabuz@gmail.com>
2021-02-26 20:09:37 +0200

    Refactore to Class components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
-import { DayComponent } from "./DayComponent";
+import DayComponent from "./DayComponent";
 
 const AppWrapper = styled.div`
   position: relative;
@@ -29,27 +29,33 @@ const AppWrapper = styled.div`
   }
 `;
 
-const App = () => {
-  const [weather, setWeaather] = useState();
-  useEffect(
-    () =>
-      fetch(
-        "http://api.weatherunlocked.com/api/forecast/50.27,30.31?app_id=231e22db&app_key=992c0bfbce0848660ff960fc64beea9c"
-      )
-        .then((response) => response.json())
-        .then((data) => setWeaather(data.Days)),
-    []
-  );
-  console.log(weather);
-  return (
-    <AppWrapper>
-      <h1>Weather in Kyiv for 7 days</h1>
-      {weather &&
-        weather.map((day) => (
-          <DayComponent key={day.date + day.moonrise_time} dayData={day} />
-        ))}
-    </AppWrapper>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { weather: [] };
+  }
+  componentDidMount() {
+    fetch(
+      "http://api.weatherunlocked.com/api/forecast/50.27,30.31?app_id=231e22db&app_key=992c0bfbce0848660ff960fc64beea9c"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ weather: data.Days })
+      })
+  }
+
+
+  render() {
+    return (
+      <AppWrapper>
+        <h1>Weather in Kyiv for 7 days</h1>
+        {this.state.weather &&
+          this.state.weather.map((day) => (
+            <DayComponent key={day.date + day.moonrise_time} dayData={day} />
+          ))}
+      </AppWrapper>
+    );
+  }
 };
 
 export default App;
--- src/DayComponent.jsx
+++ src/DayComponent.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 const DayWrapper = styled.div`
@@ -73,71 +73,80 @@ const Button = styled.button`
   }
 `;
 
-export const DayComponent = ({ dayData }) => {
-  const [showMore, setShowMore] = useState(false);
-  let date = new Date(
-    dayData.date.slice(-4),
-    (Number(dayData.date.slice(-7, -5)) - 1).toString(),
-    dayData.date.slice(-10, -8)
+class DayComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { togle: false }
+  }
+
+  date = new Date(
+    this.props.dayData.date.slice(-4),
+    (Number(this.props.dayData.date.slice(-7, -5)) - 1).toString(),
+    this.props.dayData.date.slice(-10, -8)
   );
-  const options = {
+  options = {
     weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
   };
-  return (
-    <DayWrapper>
-      <LineWrapper>
-        <p>Date: </p>
-        <p>{date.toLocaleDateString("en-EN", options)}</p>
-      </LineWrapper>
-      <LineWrapper>
-        <p>Max.temperature: </p>
-        <p>{dayData.temp_max_c} °C</p>
-      </LineWrapper>
-      <LineWrapper>
-        <p>Min.temperature: </p>
-        <p>{dayData.temp_min_c} °C</p>
-      </LineWrapper>
-      <LineWrapper>
-        <p>Humidity: </p>
-        <p>{dayData.humid_min_pct} %</p>
-      </LineWrapper>
-      <LineWrapper>
-        <p>Pressure: </p>
-        <p>{Math.floor((dayData.slp_min_mb / 1013.25) * 760)} mmHg</p>
-      </LineWrapper>
-      <LineWrapper>
-        <p>Wind speed: </p>
-        <p>{dayData.windspd_max_ms} m/s</p>
-      </LineWrapper>
-      <Button onClick={() => setShowMore(!showMore)}>
-        {showMore ? "Less" : "More"}
-      </Button>
-      {showMore && (
-        <Hourly>
-          {dayData.Timeframes.map((timeFrame, i) => {
-            return (
-              <Frame key={timeFrame.date + i}>
-                <div>
-                  <p>
-                    {i * 3}:00 - {(i + 1) * 3}:00
+
+  render() {
+    return (
+      <DayWrapper>
+        <LineWrapper>
+          <p>Date: </p>
+          <p>{this.date.toLocaleDateString("en-EN", this.options)}</p>
+        </LineWrapper>
+        <LineWrapper>
+          <p>Max.temperature: </p>
+          <p>{this.props.dayData.temp_max_c} °C</p>
+        </LineWrapper>
+        <LineWrapper>
+          <p>Min.temperature: </p>
+          <p>{this.props.dayData.temp_min_c} °C</p>
+        </LineWrapper>
+        <LineWrapper>
+          <p>Humidity: </p>
+          <p>{this.props.dayData.humid_min_pct} %</p>
+        </LineWrapper>
+        <LineWrapper>
+          <p>Pressure: </p>
+          <p>{Math.floor((this.props.dayData.slp_min_mb / 1013.25) * 760)} mmHg</p>
+        </LineWrapper>
+        <LineWrapper>
+          <p>Wind speed: </p>
+          <p>{this.props.dayData.windspd_max_ms} m/s</p>
+        </LineWrapper>
+        <Button onClick={() => this.setState({ togle: !this.state.togle })}>
+          {this.state.togle ? "Less" : "More"}
+        </Button>
+        {this.state.togle && (
+          <Hourly>
+            {this.props.dayData.Timeframes.map((timeFrame, i) => {
+              return (
+                <Frame key={timeFrame.date + i}>
+                  <div>
+                    <p>
+                      {i * 3}:00 - {(i + 1) * 3}:00
                   </p>
-                  <p>Feels like: {timeFrame.feelslike_c}</p>
-                  <p>
-                    <b>{timeFrame.wx_desc}</b>
-                  </p>
-                </div>
-                <img
-                  src={`./img/${timeFrame.wx_icon.slice(0, -3)}png`}
-                  alt="Weather ilustrate"
-                />
-              </Frame>
-            );
-          })}
-        </Hourly>
-      )}
-    </DayWrapper>
-  );
+                    <p>Feels like: {timeFrame.feelslike_c}</p>
+                    <p>
+                      <b>{timeFrame.wx_desc}</b>
+                    </p>
+                  </div>
+                  <img
+                    src={`./img/${timeFrame.wx_icon.slice(0, -3)}png`}
+                    alt="Weather ilustrate"
+                  />
+                </Frame>
+              );
+            })}
+          </Hourly>
+        )}
+      </DayWrapper>
+    );
+  }
 };
+
+export default DayComponent
